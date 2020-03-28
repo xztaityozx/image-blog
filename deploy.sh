33283fd376e1eee6d544e7740197882490e68ff5
xztaityozx <taityonohibi@gmail.com>
2020-03-28 20:58:02 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
-
-hub status &> /dev/null || {echo "directory is dirty." && exit 1}
-
-rm -rf ./public && mkdir public && hub worktree prune && rm -rf .git/worktrees/public && hub worktree add -B gh-pages public origin/gh-pages && rm -rf ./public/* && hugo && cd public && hac "publishing to gh-pages" && hps
+#!/usr/bin/zsh
+rm -rf ./public && mkdir public && hub worktree prune && rm -rf .git/worktrees/public && hub worktree add -B gh-pages public origin/gh-pages && rm -rf ./public/* && hugo && cd public && hub add -A && hub commit -m "publishing to gh-pages" && hub push
